e49375984c8a890ca5f329302248ac541349a34d
Kok Jian Yi <47259076+Jayyijianyi@users.noreply.github.com>
2021-08-17 05:57:41 +0800

    Update main.dart

--- lesson9-func/main.dart
+++ lesson9-func/main.dart
@@ -1,1 +1,25 @@
+void drawTriangle(){
+  print("  /_|");
+  print(" /__|");
+  print("/___|");
+}
 
+
+void main() {
+  
+  //function or method, code that can be reuse multiple times
+  //DRY: dont repeat yourself principle 
+
+  //draw 5 triangle:
+  /*
+  drawTriangle();
+  drawTriangle();
+  drawTriangle();
+  drawTriangle();
+  drawTriangle();*/
+
+  print("Before function"); //always run main()
+  drawTriangle(); //jump to function 
+  print("Before function");
+  
+}
